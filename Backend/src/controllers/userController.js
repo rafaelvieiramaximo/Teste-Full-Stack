3aafad6b8a19db0115e898f371c2c608475df2d5
rafaelvieiramaximo <rafavmaximo@gmail.com>
2026-02-11 16:46:48 -0300

    feat: add the last methods

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -36,3 +36,50 @@ const createUser = async (req, res, next) => {
         next(error);
     }
 }
+
+const updateUser = async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        const {name, email, role} = req.body;
+
+        const checkUser = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        if(checkUser.rows.length === 0){
+            return res.status(404).json({error: 'Usuário não encontrado'});
+        }
+
+        const result = await pool.query(
+            'UPDATE users SET name = $1, email = $2, role = $3 WHERE id = $4 RETURNING *',
+            [name, email, role || null, id]
+        );
+
+        res.json(result.rows[0]);
+    }catch(error) {
+        next(error);
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try{
+        const {id} = req.params;
+
+        const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING id', [id]);
+
+        if(result.rows.lenght === 0){
+            const error = new Error('Usuário não encontrado');
+            error.status = 404;
+            return next(error);
+        }
+
+        res.json({message: 'Usuário deletado com sucesso', id: result.rows[0].id});
+    } catch (error){
+        next(error);
+    }
+}
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+}
